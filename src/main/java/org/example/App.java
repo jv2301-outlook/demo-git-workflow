13e6e9adc8749f2283664cb56b98eaded5263f61
jv2301 <jv2301@gmail.com>
2018-04-08 14:07:54 +0000

    getMessage method created in App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,13 @@ package org.example;
 /**
  * The App class.
  */
+
 public class App {
+    public static String getMessage() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(getMessage());
     }
 }
